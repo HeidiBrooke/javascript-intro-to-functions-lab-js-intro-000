f87ddbd3820cab701640edf3d6f5cc75af3c70d1
Heidi B McGinnis <heidiisbrooke@gmail.com>
2020-10-25 23:44:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,18 +18,17 @@ function logWhisper(string) {
   )
 }
 
-function sayHiToGrandma(string){
-if {
-  string.toLowerCase() === string
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string){
+    return "I can't hear you!"
 }
-  return "I can't hear you!"
-  else if {
-    string.toUpperCase() === string
-  }
-  return "YES INDEED!"
-  else if {
-    string.mixedCase() === "I love you, Grandma."
 
+  else if(string.toUpperCase() === string) {
+    return "YES INDEED!"
   }
-  return "I love you, too."
+  
+  else if (string.mixedCase() === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  
 }
