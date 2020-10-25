4a1e7e266e75e084d6096c6e3bab50dc119f22c6
Heidi B McGinnis <heidiisbrooke@gmail.com>
2020-10-25 23:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function sayHiToGrandma(string) {
   else if(string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  
+
   else if (string.mixedCase() === "I love you, Grandma.") {
     return "I love you, too."
   }
-  
+
 }
